20a0a5c7357008a416eabe8e47476076c551294c
DaoTai <ductaidao24102001.com>
2022-06-21 10:29:39 +0700

    Lesson 96: Configure Router & Layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { publicRoutes } from '~/routes';
+import { DeafaultLayout } from '~/Components/Layout';
 function App() {
-  return (
-    <div className="App">
-      <h1>Hey boiz 123892389</h1>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, i) => {
+                        const Page = route.component;
+                        let Layout = DeafaultLayout;
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={i}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
